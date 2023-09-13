9c6f220c4e7ab39831a0a452cebe08d1b8a74fe4
KobeEugene <programmingbeast07@gmail.com>
2023-09-13 00:49:51 -0700

    size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+* Return: 0 always (success)
+*/
+int main(void)
+{
+	printf("size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(S)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+	return (0);
+}
